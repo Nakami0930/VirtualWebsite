255dfefc070bfd39ef2adedc359e023ca667cf5b
Nakami0930 <s2370012@s.do-johodai.ac.jp>
2024-07-18 14:07:39 +0900

    [fix]バグ修正

--- public/js/script_main.js
+++ public/js/script_main.js
@@ -1,15 +1,83 @@
-// script.js
-document.addEventListener('DOMContentLoaded', () => {
-    const leftArrow = document.querySelector('.arrow-left img');
-    const rightArrow = document.querySelector('.arrow-right img');
+// 画像の配列
+const images = [
+    "images/onsen_img.jpg",
+    "images/onsen_img2.jpg",
+    "images/onsen_img3.jpg",
+    "images/onsen_img4.jpg",
+    "images/onsen_img5.jpg"
+];
 
-    leftArrow.addEventListener('click', () => {
-        // 左矢印がクリックされた時の処理
-        console.log('左矢印がクリックされました');
-    });
+// テキスト画像の配列
+const textImages = [
+    [
+        "images/幻想温泉.png",
+        "images/最高の一日を.png"
 
-    rightArrow.addEventListener('click', () => {
-        // 右矢印がクリックされた時の処理
-        console.log('右矢印がクリックされました');
-    });
-});
+    ],
+    [
+        "images/癒しの湯.png",
+        "images/極楽の気分.png"
+    ],
+    [
+        "images/最高のお食事.png",
+    ],
+    [
+        "images/アクセス.png",
+    ],
+    [
+        "images/お問い合わせ.png",
+    ]
+];
+
+// 画像とリンク先の配列
+const links = [
+    "public/index.html",
+    "public/onsen.html",
+    "public/food.html",
+    "public/introduction.html",
+    "public/inquiry.html"
+];
+
+function changeImage(direction) {
+    const imgElement = document.getElementById('onsen-image');
+    const textImagesElement = document.getElementById('text-images');
+    // 現在の画像のインデックスを取得
+    const currentImage = imgElement.src;
+    const currentImageFilename = currentImage.split('/').pop();
+    const currentIndex = images.indexOf("images/" + currentImageFilename);
+
+    // 新しいインデックスを計算
+    let newIndex = (currentIndex + direction) % images.length;
+    if (newIndex < 0) {
+        newIndex = images.length - 1;
+    }
+
+    // フェードアウトアニメーション
+    imgElement.classList.remove('fade');
+    textImagesElement.classList.remove('fade');
+    void imgElement.offsetWidth; // 強制的に再描画
+    imgElement.classList.add('fade');
+    textImagesElement.classList.add('fade');
+
+    // 画像を新しいものに切り替え
+    imgElement.src = images[newIndex];
+
+    // テキスト画像を新しいものに切り替え
+    const newTextImages = textImages[newIndex];
+    textImagesElement.innerHTML = newTextImages.map(src => `<img src="${src}" alt="${src}">`).join('');
+
+    // クリックイベントを更新
+    imgElement.onclick = function() {
+        window.location.href = links[newIndex];
+    }
+}
+
+// 初期化時にクリックイベントを設定
+document.getElementById('onsen-image').onclick = function() {
+    window.location.href = links[0];
+}
+
+// 8秒ごとに自動で画像を切り替える
+setInterval(function() {
+    changeImage(1);
+}, 8000);
